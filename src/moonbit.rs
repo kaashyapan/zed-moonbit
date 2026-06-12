bf08fbb97b40b439cccda315c50865cec02451b2
kaashyapan <sunder.narayanaswamy@gmail.com>
2026-06-12 19:50:24 +0530

    find moon-lsp path from worktree

--- src/moonbit.rs
+++ src/moonbit.rs
@@ -9,22 +9,29 @@ impl zed::Extension for MoonBitExtension {
 
     fn language_server_command(
         &mut self,
-        _language_server_id: &LanguageServerId,
+        language_server_id: &LanguageServerId,
         worktree: &Worktree,
     ) -> Result<zed::Command> {
-        let path = worktree.which("moon-lsp");
-        if let Some(command) = path {
-            return Ok(zed::Command {
-                command,
-                args: vec!["--stdio".to_string()],
-                env: Default::default(),
-            });
-        } else {
-            return Err(
+        if language_server_id.as_ref() == "moon-lsp" {
+            let path = worktree.which("moon-lsp");
+            if let Some(command) = path {
+                return Ok(zed::Command {
+                    command,
+                    args: vec!["--stdio".to_string()],
+                    env: Default::default(),
+                });
+            } else {
+                return Err(
                 "The binary 'moon-lsp' was not found in your PATH.\n\
                 Verify that the installation PATH is correctly exposed to Zed's startup environment.".to_string()
             );
+            }
         }
+
+        Err(format!(
+            "Unknown language server: {}",
+            language_server_id.as_ref()
+        ))
     }
 
     fn language_server_workspace_configuration(
